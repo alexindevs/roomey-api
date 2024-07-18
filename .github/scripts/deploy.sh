27d1670525c2c5704ae53d160ccbdaba6ac0187a
Alexin <alexindevs@gmail.com>
2024-07-18 01:01:44 +0100

    edit deploy script

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,6 +5,18 @@ mkdir -p ~/roomey-api
 # Navigate to the project directory
 cd ~/roomey-api
 
+# Check if npm is installed
+if ! command -v npm &> /dev/null
+then
+    echo "npm could not be found, installing nvm, Node.js, and npm"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    source ~/.bashrc
+    nvm install 20
+    nvm use 20
+fi
+
+git remote set-url origin https://github.com/alexindevs/roomey-api.git
+
 # Pull the latest changes from the main branch
 git pull origin main
 
